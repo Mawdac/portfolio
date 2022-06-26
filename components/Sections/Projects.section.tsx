f8784d8b02165dcb8fcde0e335819901c8dc5b40
Mawdac <edwardreillycomputers@gmail.com>
2022-06-26 02:02:40 -0400

    :art: style: Update wording for smol views

--- components/Sections/Projects.section.tsx
+++ components/Sections/Projects.section.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Projects: NextComponentType = () => {
   return (
     <div className="my-16 px-3 font-sen" id="projects">
-      <p className="text-3xl font-bold text-white">Featured projects</p>
+      <p className="text-3xl font-bold text-white">Featured Projects</p>
       <div className="my-8 flex flex-col items-center justify-center gap-10 sm:flex-row">
         <Link href="https://github.com/Mawdac/github-actions-sandbox" passHref>
           <a
@@ -14,8 +14,8 @@ const Projects: NextComponentType = () => {
             rel="noopener noreferrer"
           >
             <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
-              <p className="text-xl font-semibold">ci/cd sandbox</p>
-              <p>a sandbox for tinkering with github actions</p>
+              <p className="text-xl font-semibold">sandbox</p>
+              <p>tinkering with github actions</p>
             </div>
           </a>
         </Link>
@@ -27,8 +27,8 @@ const Projects: NextComponentType = () => {
             rel="noopener noreferrer"
           >
             <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
-              <p className="text-xl font-semibold">ci/cd sandcastle</p>
-              <p>for playing with repo-to-repo shenanigans</p>
+              <p className="text-xl font-semibold">sandcastle</p>
+              <p>for repo-to-repo shenanigans</p>
             </div>
           </a>
         </Link>
@@ -41,7 +41,7 @@ const Projects: NextComponentType = () => {
           >
             <div className="flex h-full w-full flex-col items-center justify-center rounded-lg bg-primary px-2 text-center font-medium">
               <p className="text-xl font-semibold">rg280v theme</p>
-              <p>fork of a megapixels theme for the rg280v</p>
+              <p>my fork of megapixels </p>
             </div>
           </a>
         </Link>
